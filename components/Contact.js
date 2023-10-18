faf5e463cd17df8c1dd28bf3b7a3f50f2092b6d6
mathieumunyakazimoise <m.munyakazi1@alustudent>
2023-10-17 21:50:40 -0700

    nnh

---
+++ components/Contact.js
@@ -0,0 +1,67 @@
+import Image from 'next/image';
+import { contact } from '@/data/config';
+
+export default function Stack() {
+  return (
+    <div>
+      <div className="overflow-x-hidden w-full">
+        <h2 className="landingSectionTitle max-w-max mx-0 text-left relative mb-4 md:w-max ">
+          {contact.title}
+        </h2>
+      </div>
+      <p>
+        Shoot me an email:{' '}
+        <a
+          className="dark:text-white text-black transition-colors duration-500"
+          href={`mailto:${contact.email}`}
+        >
+          {contact.email}
+        </a>
+      </p>
+      <div className="flex space-x-5 mt-5 text-lightText transition-colors duration-500">
+        {contact.github && (
+          <a
+            href={`https://github.com/${contact.github}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/static/icons/github.svg"
+              width={30}
+              height={30}
+              alt="Github icon"
+            />
+          </a>
+        )}
+        {contact.telegram && (
+          <a
+            href={`https://t.me/${contact.telegram}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/static/icons/telegram.svg"
+              width={30}
+              height={30}
+              alt="Telegram icon"
+            />
+          </a>
+        )}
+        {contact.linkedin && (
+          <a
+            href={`https://linkedin.com/in/${contact.linkedin}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/static/icons/linkedin.svg"
+              width={30}
+              height={30}
+              alt="LinkedIn icon"
+            />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
