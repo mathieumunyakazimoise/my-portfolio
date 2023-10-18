9d82c86ce41e5491e5b8161cfb255b257822b08e
mathieumunyakazimoise <m.munyakazi1@alustudent>
2023-10-17 23:05:53 -0700

    yesu

---
+++ data/config.js
@@ -0,0 +1,57 @@
+export const meta = {
+  title: 'Home | Mathieu',
+  description: 'Hey, I\'m Mathieu. I\'m a software developer and designer building things people love.'
+}
+
+export const hero = {
+  title: (
+    <div>
+      Hey there 👋! <br /> I'm Mathieu Munyakazi Moise.
+    </div>
+  ),
+  desc: (
+    <p>
+      I'm a passionate software developer and designer with a knack for creating digital experiences that people love. I'm on a mission to craft software that not only functions flawlessly but also delights users with its aesthetics and usability.<br />
+      I specialize in designing eye-catching, minimalist user interfaces. UIs should be intuitive, visually appealing, and easy to interact with; to me, simplicity is sophistication.
+    </p>
+  )
+};
+
+export const projects = {
+  title: 'My Projects',
+  desc: "Here are some of my favorite projects that I've worked on:",
+  projects: [
+    {
+      title: 'medie',
+      description: 'A python package that automates basic git functions; add, commit & push.',
+      link: '',
+      github: 'mathieumunyakazimoise/medie'
+    },
+    
+  ]
+};
+
+export const stack = {
+  title: 'My Stack',
+  stack: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Typescript',
+    'React',
+    'NextJS',
+    'GatsbyJS',
+    'TailwindCSS',
+    'Python',
+    'PostgreSQL',
+    'Git'
+  ]
+};
+
+export const contact = {
+  title: 'Contact Me',
+  email: 'moisemunyakazi@gmail.com',
+  github: 'mathieumunyakazimoise',
+  instagram: 'munyakazimoise
+  linkedin: 'munyakazi-moise-619a5a255'
+};
